3b227d61071784aac2f242a602841e0e3b2ea20f
Jérémie Zarca <jzarca01@users.noreply.github.com>
2020-12-27 18:34:04 +0100

    Update lilo-preinstall.sh

--- lilo-preinstall.sh
+++ lilo-preinstall.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo apt-get install bluetooth bluez libbluetooth-dev libudev-dev libcap2-bin
+sudo apt-get install bluetooth bluez libbluetooth-dev libudev-dev libcap2-bin libusb-dev
 sudo setcap cap_net_raw+eip $(eval readlink -f `which node`)
